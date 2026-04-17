8377a49ebc4c4a96dcdd8a610b465a116e8b2ce9
Abby Hartley <97635153+abbyhartley@users.noreply.github.com>
2026-04-17 11:46:03 -0700

    Update __init__.py

--- src/normflow/__init__.py
+++ src/normflow/__init__.py
@@ -1,2 +1,5 @@
 # normflow is a python package that will contain all the functions I frequenctly reuse in this modeling project
 # I'll import these functions as a package and call them in Sherlock
+from .selection import DESISelectionConfig, desi_bgs_training_mask
+
+__all__ = ["DESISelectionConfig", "desi_bgs_training_mask"]
